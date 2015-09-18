0097b309b706f037772c61099775b23824e10912
Deon Heyns <heynsd@gmail.com>
2015-09-18 18:19:29 -0300

    Increased limit of points

--- app/forms.py
+++ app/forms.py
@@ -8,4 +8,4 @@ class PointsInRadiusForm(Form):
     latitude = FloatField('latitude', validators=[NumberRange(-999, 999), DataRequired()])
     longitude = FloatField('longitude', validators=[NumberRange(-999, 999), DataRequired()])
     radius = FloatField('radius', validators=[NumberRange(0, 4000), DataRequired()])
-    num_of_points = FloatField('num_of_points', validators=[NumberRange(0, 360, "Range between 0 - 360"), DataRequired()])
+    num_of_points = FloatField('num_of_points', validators=[NumberRange(0, 9999, "Range between 0 - 1000"), DataRequired()])
